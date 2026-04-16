563f3708846d4b7658a8d8075a22b1f4da063bf7
SUKANT43 <sukant98657@gmail.com>
2026-04-16 11:02:08 +0530

    supe admin login data strucutre added

---
+++ DataStrucutre/DataStructure/SuperAdminRegistrsation.cs
@@ -0,0 +1,29 @@
+﻿using DataStrucutre.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStrucutre.DataStructure
+{
+    public class SuperAdminRegistrsation
+    {
+
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public RegistrationStatus Status { get; set; }
+        public UserLevel UserLevel { get; }
+        public SuperAdminRegistrsation(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            Status = RegistrationStatus.Pending;
+            UserLevel = UserLevel.SuperAdmin;
+        }
+    }
+}
+
---
+++ DataStrucutre/Enums/DataEnums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStrucutre.Enums
+{
+    public enum RegistrationStatus
+    {
+        Pending,
+        Completed
+    }
+
+    public enum UserLevel
+    {
+        SuperAdmin,
+        Admin,
+        User
+    }
+
+    public enum StockInformation
+    {
+        InStock,
+        LowStock,
+        OutOfStock
+    }
+
+}
