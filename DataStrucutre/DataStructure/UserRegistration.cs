c09aef91789a25f5a9fb086bbaf59eee510d1a42
SUKANT43 <sukant98657@gmail.com>
2026-04-16 11:07:57 +0530

    user registration

---
+++ DataStrucutre/DataStructure/UserRegistration.cs
@@ -0,0 +1,24 @@
+﻿using DataStrucutre.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStrucutre.DataStructure
+{
+    class UserRegistration
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public UserLevel UserLevel { get; set; }
+        public UserRegistration(string name,string email,string password,UserLevel userLevel)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            UserLevel = userLevel;
+        }
+    }
+}
